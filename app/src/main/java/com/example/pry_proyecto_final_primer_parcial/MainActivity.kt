80e70cfd70b61c528ceb7b544911d5882cd68241
Christopher Iza <cdiza5@espe.edu.ec>
2023-12-09 19:14:44 -0500

    Inicio

---
+++ app/src/main/java/com/example/pry_proyecto_final_primer_parcial/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pry_proyecto_final_primer_parcial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
