44352f748ab802a064f227d3cc10fc99090f92f5
Christopher Iza <cdiza5@espe.edu.ec>
2023-12-09 22:34:52 -0500

    Juegos

---
+++ app/src/main/java/com/example/pry_proyecto_final_primer_parcial/Memoria.kt
@@ -0,0 +1,11 @@
+package com.example.pry_proyecto_final_primer_parcial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Memoria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memoria)
+    }
+}
