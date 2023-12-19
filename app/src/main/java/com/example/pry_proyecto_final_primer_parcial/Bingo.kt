83ee37c069dd6fb0a8ac21d8d1ae8d721e3a87aa
Chaps0218 <mathiguevara@gmail.com>
2023-12-18 21:22:50 -0500

    Funcional el bingo, no bonito

--- app/src/main/java/com/example/pry_proyecto_final_primer_parcial/Bingo.kt
+++ app/src/main/java/com/example/pry_proyecto_final_primer_parcial/Bingo.kt
@@ -1,7 +1,12 @@
 package com.example.pry_proyecto_final_primer_parcial
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -23,8 +28,7 @@ class Bingo : AppCompatActivity() {
     private lateinit var matrizResultados: Array<Array<Int>>
     private lateinit var matrizRestas: Array<Array<Int>>
     private lateinit var matrizRestasString: Array<Array<String>>
-
-
+    private lateinit var posicionesVistas : Array<Array<Boolean>>
 
     private var posicionActual = Pair(0, 0)
 
@@ -66,9 +70,17 @@ class Bingo : AppCompatActivity() {
 
         matrizRestasString = Array(3) { i ->
             Array(3) { j ->
-                "${matrizRestas[i][j]} - ${matrizResultados[i][j]}"
+                "${matrizResultados[i][j]} - ${matrizRestas[i][j]}"
             }
         }
+
+        posicionesVistas = Array(3) { i ->
+            Array(3) { j ->
+                false
+            }
+        }
+
+        cambiarPosicionActual()
 
         lblNum1.text = matrizOriginal[0][0].toString()
         lblNum2.text = matrizOriginal[0][1].toString()
@@ -81,8 +93,61 @@ class Bingo : AppCompatActivity() {
         lblNum9.text = matrizOriginal[2][2].toString()
     }
 
-    fun check(){
+    fun cambiarPosicionActual(){
+        val num1 = Random.nextInt(0, 3)
+        val num2 = Random.nextInt(0, 3)
 
+        if (posicionesVistas.all { fila -> fila.all { it } }) {
+            mostrarPopupVictoria()
+            lblEcuacion.text = "¡Ganaste!"
+            return
+        }
+
+        while (posicionesVistas[num1][num2]){
+            cambiarPosicionActual()
+            return
+        }
+        posicionActual = Pair(num1, num2)
+        lblEcuacion.text = matrizRestasString[num1][num2]
     }
 
+    fun check(v: View){
+        val button = findViewById<TextView>(v.id)
+        val hint = button.hint.toString()
+        println(hint)
+        val num1 = hint.split(" ")[0].toInt()
+        val num2 = hint.split(" ")[1].toInt()
+
+        if (num1 == posicionActual.first && num2 == posicionActual.second){
+            Toast.makeText(this, "Correcto", Toast.LENGTH_SHORT).show()
+            posicionesVistas[num1][num2] = true
+            button.text = "✓"
+            cambiarPosicionActual()
+            lblEcuacion.text = matrizRestasString[posicionActual.first][posicionActual.second]
+        } else {
+            Toast.makeText(this, "Incorrecto", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun mostrarPopupVictoria() {
+        val mDialog = Dialog(this)
+        mDialog.setContentView(R.layout.popup_victoria)
+        mDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val buttonJugarDeNuevo = mDialog.findViewById<Button>(R.id.btn_jugar_de_nuevo)
+        val buttonSalir = mDialog.findViewById<Button>(R.id.btn_Salir)
+
+        buttonJugarDeNuevo.setOnClickListener {
+            mDialog.dismiss()
+            recreate()
+        }
+
+        buttonSalir.setOnClickListener {
+            finish()
+        }
+
+        mDialog.show()
+        mDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+    fun salir(v: View){
+        finish()
+    }
 }
